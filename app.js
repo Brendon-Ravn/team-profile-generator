7917e46bbc24a818bf4f3de6a55e72c150d5f386
Brendon Conatser <b.conatser21@gmail.com>
2020-09-15 16:42:05 -0500

    wrote the writing code in the app file

--- app.js
+++ app.js
@@ -56,3 +56,53 @@ const managerQuestions = [
     message: "Manager's office number:"
   }
 ];
+
+function engineerInfo() {
+  inquirer.prompt(engineerQuestions)
+    .then((response) => {
+      const engineer = new Engineer(response.name, response.id, response.email, response.github);
+      team.push(engineer);
+      employeeInfo();
+    });
+}
+
+function internInfo() {
+  inquirer.prompt(internQuestions)
+    .then((response) => {
+      const intern = new Intern(response.name, response.id, response.email, response.school);
+      team.push(intern);
+      employeeInfo();
+    });
+}
+
+function createHTML(){
+  try {
+    const html = render(team);
+    fs.writeFileSync(outputPath, html);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function employeeInfo() {
+  const whichEmp = [
+    {
+      name: 'choice',
+      type: 'checkbox',
+      message: 'What employee type are we adding?',
+      choices: ['Engineer', 'Intern', 'Finished']
+    }
+  ];
+  inquirer.prompt(whichEmp)
+    .then((answers) => {
+      if (answers.choice === 'Engineer') {
+        engineerInfo();
+      }
+      if (answers.choice === 'Intern') {
+        internInfo();
+      }
+      if (answers.choice === 'Finished') {
+        createHTML();
+      }
+    });
+}
