564b7cbe2a76814927876b1cf16c3ffa4a70ed78
Brendon Conatser <b.conatser21@gmail.com>
2020-09-15 16:02:06 -0500

    wrote questions for the team members

--- app.js
+++ app.js
@@ -10,26 +10,49 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "Employee's name:"
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "Employee's id:"
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Employee's email:"
+  }
+];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const engineerQuestions = [
+  ...questions,
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+  }
+];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+const internQuestions = [
+  ...questions,
+  {
+    type: 'input',
+    name: 'school',
+    message: 'What is your current or most recent institute of education?'
+  }
+];
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const managerQuestions = [
+  ...questions,
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: "Manager's office number:"
+  }
+];
